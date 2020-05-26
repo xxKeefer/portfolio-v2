da061689a6807cd9a512d5a81cf2e4bdfe38a147
xxKeefer <xxkeefer.code@gmail.com>
2020-05-26 22:59:13 +1000

    set up layout for glider

---
+++ js/app.js
@@ -0,0 +1,12 @@
+
+
+//glider config
+new Glider(document.querySelector('.glider'), {
+    slidesToShow: 1,
+    draggable: true,
+    dots: '#dots',
+    arrows: {
+      prev: '.glider-prev',
+      next: '.glider-next'
+    }
+  })
